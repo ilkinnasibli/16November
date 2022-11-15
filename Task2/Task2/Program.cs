0643c69b0ec3a0c1e2de99c310513e678885c837
ilkinnasibli <ilkinnasibli@mail.ru>
2022-11-16 00:31:06 +0400

    homework

---
+++ Task2/Task2/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          
+
+            int num1;
+            int sum = 0;
+            Console.WriteLine("Arrayin uzunlugunu daxil edin :");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            int[] array = new int[num1];
+            for (int i = 0; i < num1; i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            foreach (var item in array)
+            {
+                if (item % 2 == 0)
+                {
+                    sum = sum + item;
+
+
+                }
+            }
+            Console.WriteLine("Cut ededlerin cemi :");
+            Console.WriteLine(sum);
+        }
+    }
+}
